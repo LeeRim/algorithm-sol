cb398dbd5349f5ef2a661065097e43c41c825dcc
LeeRim <yrim113@gmail.com>
2023-03-06 13:32:49 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 3.04 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,17 +1,16 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        String[] arr = s.split(" ");
-        List<Integer> iarr = Arrays.stream(arr).map(str -> Integer.valueOf(str)).sorted().collect(Collectors.toList());
-        int max = iarr.get(iarr.size()-1);
-        int min = iarr.get(0);
-        answer = String.valueOf(min) + " " + String.valueOf(max);
-        // System.out.println(answer);
-        return answer;
+        int[] ints = Arrays.stream(s.split(" "))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(Arrays.stream(ints).min().getAsInt());
+        sb.append(" ");
+        sb.append(Arrays.stream(ints).max().getAsInt());
+
+        return sb.toString();
     }
 }
