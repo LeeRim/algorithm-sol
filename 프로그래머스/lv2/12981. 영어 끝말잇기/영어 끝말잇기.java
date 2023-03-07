107e6157aab49877621a75c8edbaaa41a43add86
LeeRim <yrim113@gmail.com>
2023-03-07 17:31:35 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] result = new int[2];
+
+        Set<String> answer = new HashSet<>();
+
+        int index = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() == 1) {
+                index = i;
+                break;
+            }
+            if (!answer.add(words[i])) {
+                index = i;
+                break;
+            }
+            if (i > 0 && words[i].charAt(0) != words[i - 1].charAt(words[i - 1].length() - 1)) {
+                index = i;
+                break;
+            }
+        }
+        index++;
+
+        if (words.length != answer.size()) {
+            if (index % n == 0) {
+                result[0] = n;
+                result[1] = index / n;
+            } else {
+                result[0] = index % n;
+                result[1] = index / n + 1;
+            }
+        }
+
+//        System.out.println(Arrays.toString(result));
+        return result;
+    }
+}
