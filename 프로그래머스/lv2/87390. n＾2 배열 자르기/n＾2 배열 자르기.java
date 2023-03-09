8e391c119910724cafca88a983f215ad5b132d6f
LeeRim <yrim113@gmail.com>
2023-03-09 14:22:08 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.75 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,28 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int[] solution(int n, long left, long right) {
+        long m = (long) n;
+        int[] arr = new int[(int) (right - left) + 1];
 
-        int leftx = (int) (left / n);
-        int lefty = (int) (left % n);
-        int rightx = (int) (right / n);
-        int righty = (int) (right % n);
-        
-        List<Integer> list = new ArrayList<>();
-        for (int i = leftx; i <= rightx; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == rightx && j > righty) {
-                    break;
-                }
-                if (i == leftx && j < lefty) {
-                    continue;
-                }
-                list.add(Math.max(i + 1, j + 1));
-            }
+        for (long i = left; i <= right; i++) {
+            arr[(int) (i - left)] = (int) Math.max(i / m + 1, i % m + 1);
         }
-        
-//        System.out.println(list);
-        return list.stream().mapToInt(Integer::intValue).toArray();
+
+        return arr;
     }
 }
