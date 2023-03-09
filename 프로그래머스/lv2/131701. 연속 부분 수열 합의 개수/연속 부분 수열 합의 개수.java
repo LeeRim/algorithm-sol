1bf01dddb70bd0fa1991a8165fba26b2c0ed696a
LeeRim <yrim113@gmail.com>
2023-03-09 18:06:05 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 128.94 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,27 +3,22 @@ import java.util.Set;
 
 class Solution {
     public int solution(int[] elements) {
-        Set<Integer> sums = new HashSet<>();
-        for (int element : elements) {
-            sums.add(element);
+
+        int[] elementDouble = new int[elements.length * 2];
+        for (int i = 0; i < elementDouble.length; i++) {
+            elementDouble[i] = elements[i % elements.length];
         }
-        int[][] dp = new int[elements.length][elements.length];
-        dp[0] = elements.clone();
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 0; j < dp[0].length; j++) {
-                if (i + j >= dp[0].length) {
-                    dp[i][j] = dp[i - 1][j] + dp[0][i + j - dp[0].length];
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[0][i + j];
-                }
-                sums.add(dp[i][j]);
+
+        Set<Integer> sums = new HashSet<>();
+        int sum = 0;
+        for (int i = 0; i < elements.length; i++) {
+            sum = 0;
+            for (int j = 0; j < elements.length; j++) {
+                sum += elementDouble[i + j];
+                sums.add(sum);
             }
         }
-//        for (int[] ints : dp) {
-//            System.out.println(Arrays.toString(ints));
-//        }
-//        System.out.println(sums);
-//        System.out.println(sums.size());
+
         return sums.size();
     }
 }
