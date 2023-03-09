e878dcf63249b8138c50af849094f330c06a0b88
LeeRim <yrim113@gmail.com>
2023-03-09 18:19:05 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 292.91 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,18 +3,16 @@ import java.util.Set;
 
 class Solution {
     public int solution(int[] elements) {
-
-        int[] elementDouble = new int[elements.length * 2];
-        for (int i = 0; i < elementDouble.length; i++) {
-            elementDouble[i] = elements[i % elements.length];
-        }
-
         Set<Integer> sums = new HashSet<>();
         int sum = 0;
         for (int i = 0; i < elements.length; i++) {
             sum = 0;
             for (int j = 0; j < elements.length; j++) {
-                sum += elementDouble[i + j];
+                if (i + j >= elements.length) {
+                    sum += elements[(i + j) % elements.length];
+                } else {
+                    sum += elements[i + j];
+                } 
                 sums.add(sum);
             }
         }
