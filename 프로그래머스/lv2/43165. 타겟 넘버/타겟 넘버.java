a783b81bb24ca83472382f607043b82f3830fd07
LeeRim <yrim113@gmail.com>
2023-03-10 15:37:52 +0900

    [level 2] Title: 타겟 넘버, Time: 0.56 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,40 +1,20 @@
-import java.util.*;
-
 class Solution {
-    private int answer;
-
+        int answer;
     public int solution(int[] numbers, int target) {
         answer = 0;
-
-        dfs(numbers, target, new ArrayList<>());
-
+        getSum(numbers, 0, 0, target);
         return answer;
     }
 
-    public void dfs(int[] numbers, int target, List<String> operater) {
-        if (numbers.length == operater.size()) {
-            checkSum(numbers, target, operater);
+    public void getSum(int[] numbers, int index, int sum, int target) {
+        if (numbers.length == index) {
+            if (sum == target) {
+                answer++;
+            }
             return;
         }
-
-        operater.add("+");
-        dfs(numbers, target, new ArrayList<>(operater));
-        operater.set(operater.size() - 1, "-");
-        dfs(numbers, target, new ArrayList<>(operater));
-    }
-
-    public void checkSum(int[] numbers, int target, List<String> operater) {
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (operater.get(i).equals("+")) {
-                sum += numbers[i];
-            } else {
-                sum -= numbers[i];
-            }
-        }
-
-        if (sum == target) {
-            answer++;
-        }
+        
+        getSum(numbers, index + 1, sum + numbers[index], target);
+        getSum(numbers, index + 1, sum - numbers[index], target);
     }
 }
