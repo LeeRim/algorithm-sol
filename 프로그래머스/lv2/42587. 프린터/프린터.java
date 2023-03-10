86208ab8da6b474102d5fc08ac6eb1ef1cc5e5b5
LeeRim <yrim113@gmail.com>
2023-03-10 15:20:10 +0900

    [level 2] Title: 프린터, Time: 0.82 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.java
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -1,47 +1,36 @@
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.Deque;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        int myLocation = location;
-
-        Queue<Integer> orders = new LinkedList<>();
-        PriorityQueue<Integer> printOrders = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        Deque<Integer> waits = new ArrayDeque<>();
         for (int priority : priorities) {
-            int p = Integer.valueOf(priority);
-            orders.add(p);
-            printOrders.add(p);
+            priorityQueue.add(priority);
+            waits.add(priority);
         }
 
-        while (!orders.isEmpty()) {
-            int first = orders.poll();
-//            System.out.println("now : " + first);
-//            System.out.println("max : " + printOrders.peek());
-            if (first == printOrders.peek()) {
+        int answer = 0;
+        while (!waits.isEmpty()) {
+            if (priorityQueue.peek() == waits.getFirst()) {
                 answer++;
-                printOrders.remove();
-                if (myLocation == 0) {
+                waits.poll();
+                priorityQueue.poll();
+                if (location == 0) {
                     break;
-                } else {
-                    myLocation--;
                 }
-            } else {
-                orders.add(first);
-                if (myLocation == 0) {
-                    myLocation = orders.size() - 1;
-                } else {
-                    myLocation--;
-                }
+                location--;
+                continue;
             }
-//            System.out.println("me : " + myLocation);
-//            System.out.println("!asr : " + answer);
+            waits.add(waits.poll());
+            if (location == 0) {
+                location = waits.size() - 1;
+                continue;
+            }
+            location--;
         }
-
-//        System.out.println(answer);
-
         return answer;
     }
 }
