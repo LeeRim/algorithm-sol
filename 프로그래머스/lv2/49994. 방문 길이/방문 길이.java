a2934d878dcefb09dfe8c8b9063b0112d1f22e07
LeeRim <yrim113@gmail.com>
2023-03-14 15:52:52 +0900

    [level 2] Title: 방문 길이, Time: 1.45 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
@@ -1,69 +1,57 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 class Solution {
-    private int[] dx = {0, 0, -1, 1};
-    private int[] dy = {1, -1, 0, 0};
-    private char[] reverse = {'D', 'U', 'R', 'L'};
-
     public int solution(String dirs) {
-        Map<Character, Integer> indexs = new HashMap<>();
-        indexs.put('U', 0);
-        indexs.put('D', 1);
-        indexs.put('L', 2);
-        indexs.put('R', 3);
+        Map<Character, Integer> dirIndex = new HashMap<>();
+        dirIndex.put('U', 0);
+        dirIndex.put('D', 1);
+        dirIndex.put('L', 2);
+        dirIndex.put('R', 3);
 
-        Set<Way> visited = new HashSet<>();
-        //현재 좌표
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {1, -1, 0, 0};
+
+        Set<String> visited = new HashSet<>();
+        int count = 0;
         int x = 0;
         int y = 0;
+        int index, nx, ny;
+        StringBuilder visit;
         for (int i = 0; i < dirs.length(); i++) {
-            int index = indexs.get(dirs.charAt(i));
-            //다음 좌표
-            int nx = x + dx[index];
-            int ny = y + dy[index];
-            //좌표 범위를 넘어가면 무시
+            index = dirIndex.get(dirs.charAt(i));
+            nx = x + dx[index];
+            ny = y + dy[index];
+
             if (nx < -5 || ny < -5 || nx > 5 || ny > 5) {
                 continue;
             }
-            Way right = new Way(x, y, dirs.charAt(i));
-            x = nx;
-            y = ny;
-            //방문했던 Way면 다음 좌표 실행
-            if (visited.contains(right)) {
+
+            visit = new StringBuilder();
+            visit.append(x)
+                    .append(y)
+                    .append(nx)
+                    .append(ny);
+            if (visited.contains(visit.toString())) {
+                x = nx;
+                y = ny;
                 continue;
             }
-            //Way를 방문 set에 추가
-            visited.add(right); //현재 좌표 + 방향
-            visited.add(new Way(nx, ny, reverse[index])); //다음 좌표 + 역방향
+            visited.add(visit.toString());
+            visit = new StringBuilder();
+            visit.append(nx)
+                    .append(ny)
+                    .append(x)
+                    .append(y);
+            visited.add(visit.toString());
+            count++;
+
+            x = nx;
+            y = ny;
         }
 
-        int count = visited.size() / 2;
         return count;
-    }
-
-    class Way {
-
-        int x;
-        int y;
-        char direction;
-
-        public Way(int x, int y, char direction) {
-            this.x = x;
-            this.y = y;
-            this.direction = direction;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Way way = (Way) o;
-            return x == way.x && y == way.y && direction == way.direction;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y, direction);
-        }
     }
 }
