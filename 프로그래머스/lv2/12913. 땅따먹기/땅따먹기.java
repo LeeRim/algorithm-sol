55ddef08c9719770359a7de7e7d90fc35851172f
LeeRim <yrim113@gmail.com>
2023-03-14 15:17:34 +0900

    [level 2] Title: 땅따먹기, Time: 26.96 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -1,22 +1,28 @@
-import java.util.Arrays;
-
 class Solution {
-
     int solution(int[][] land) {
         int[][] dp = new int[land.length][4];
-        dp[0] = land[0].clone();
+        for (int i = 0; i < land.length; i++) {
+            dp[i] = land[i].clone();
+        }
+
+        int max;
         for (int i = 1; i < dp.length; i++) {
             for (int j = 0; j < 4; j++) {
+                max = -1;
                 for (int k = 0; k < 4; k++) {
                     if (j == k) {
                         continue;
                     }
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][k]);
+                    max = Math.max(max, dp[i - 1][k]);
                 }
-                dp[i][j] += land[i][j];
+                dp[i][j] += max;
             }
         }
 
-        return Arrays.stream(dp[land.length - 1]).max().getAsInt();
+        int answer = -1;
+        for (int i = 0; i < 4; i++) {
+            answer = Math.max(answer, dp[dp.length - 1][i]);
+        }
+        return answer;
     }
 }
