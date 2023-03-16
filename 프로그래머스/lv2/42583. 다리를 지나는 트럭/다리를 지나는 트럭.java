03cf122167e08f3707d76fb3df0ba9de2bc2e9eb
LeeRim <yrim113@gmail.com>
2023-03-16 15:38:32 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.11 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,39 +1,33 @@
 import java.util.ArrayDeque;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Deque;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        Queue<Integer> trucks = new LinkedList<Integer>();
-        for (int t : truck_weights) {
-            trucks.add(t);
-        }
-        ArrayDeque<Integer> bridge_trucks = new ArrayDeque<Integer>();
-        bridge_trucks.add(0);
-        int in_bridge = 0;
-        int truck = 0;
-        while (bridge_trucks.peek() != -1) {
-            if (bridge_trucks.size() == bridge_length) {
-                truck = bridge_trucks.poll();
-                in_bridge -= truck;
-            }
-            if (!trucks.isEmpty()) {
-                if ((in_bridge + trucks.peek()) <= weight) {
-                    truck = trucks.poll();
-                    in_bridge += truck;
-                    bridge_trucks.add(truck);
-                } else {
-                    bridge_trucks.add(0);
-                }
-            } else {
-                bridge_trucks.add(-1);
-            }
 
-//            System.out.println(bridge_trucks);
-            answer++;
+        Deque<Integer> bridge = new ArrayDeque<>();
+        for (int i = 0; i < bridge_length; i++) {
+            bridge.add(0);
         }
-        // System.out.println(answer);
-        return answer;
+
+        int bridge_weight = weight;
+        int truck = 0;
+        int count = 0;
+        int done;
+        while (truck < truck_weights.length) {
+            count++;
+            done = bridge.poll();
+            if (done > 0) {
+                bridge_weight += done;
+            }
+            if (bridge_weight >= truck_weights[truck]) {
+                bridge.add(truck_weights[truck]);
+                bridge_weight -= truck_weights[truck];
+                truck++;
+            } else {
+                bridge.add(0);
+            }
+        }
+
+        return count + bridge_length;
     }
 }
