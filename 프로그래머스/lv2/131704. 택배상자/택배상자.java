ee02540dd62ef10aa2f1adf7778429c2d01d2b6f
LeeRim <yrim113@gmail.com>
2023-03-29 11:12:53 +0900

    [level 2] Title: 택배상자, Time: 20.46 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.java
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -1,22 +1,31 @@
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public int solution(int[] order) {
-        int cnt = 0;
+        int answer = 0;
 
-        Stack<Integer> 보조 = new Stack<>();
-
-        for(int i=0; i<order.length; i++){
-            보조.add(i+1);
-
-            while(!보조.isEmpty()){
-                if(보조.peek() == order[cnt]){
-                    보조.pop();
-                    cnt++;
-                }else break;
-
+        List<Integer> sub = new ArrayList<>();
+        int box = 1;
+        for (int i = 0; i < order.length; i++) {
+            if (box == order[i]) {
+                answer++;
+                box++;
+                continue;
             }
+            if (!sub.isEmpty() && sub.get(sub.size() - 1) == order[i]) {
+                answer++;
+                sub.remove(sub.size() - 1);
+                continue;
+            }
+            sub.add(box);
+            box++;
+            if (box > order.length) {
+                break;
+            }
+            i--;
         }
-        return cnt;
+
+        return answer;
     }
 }
